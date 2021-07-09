8755cfaf35af9794d27fbe0a0777948b62415161
bartoszluka <bartoszluka1@gmail.com>
2021-07-10 01:21:15 +0200

    Add correct directions

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
+module Main exposing (Direction(..), GameState(..), Model, Msg(..), Point, Size, Snake, arrows, board, currentSnake, init, initialModel, initialSnake, main, moveSnake, playButton, subscriptions, tile, update, validateSnake, view, viewLayout, white)
 
 import Browser
 import Element exposing (..)
@@ -31,168 +31,20 @@ main =
 
 type alias Model =
     { snake : Snake
-    , playing : Bool
+    , gameState : GameState
     , size : Size
     }
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( Model startingSnake True (Size 1 10)
-    , Cmd.none
-    )
+initialModel : Model
+initialModel =
+    Model initialSnake Playing (Size 1 10)
 
 
-
--- UPDATE
-
-
-type Msg
-    = Tick Time.Posix
-    | SetPlaying Bool
-    | ChangeDirection Direction
-
-
-validateSnake : Size -> Snake -> Bool
-validateSnake size snake =
-    (snake.head.x <= size.max)
-        && (snake.head.x >= size.min)
-        && (snake.head.y <= size.max)
-        && (snake.head.y >= size.min)
-
-
-moveSnake : Snake -> Snake
-moveSnake { body, direction, head } =
-    let
-        tail =
-            body |> List.tail |> Maybe.withDefault startingSnake.body
-
-        updateBody newHead =
-            Snake (tail ++ [ newHead ]) newHead direction
-    in
-    case direction of
-        Right ->
-            Point (head.x + 1) head.y |> updateBody
-
-        Left ->
-            Point (head.x - 1) head.y |> updateBody
-
-        Up ->
-            Point head.x (head.y - 1) |> updateBody
-
-        Down ->
-            Point head.x (head.y + 1) |> updateBody
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Tick _ ->
-            let
-                newSnake =
-                    moveSnake model.snake
-            in
-            if model.playing && validateSnake model.size newSnake then
-                ( { model | snake = newSnake }
-                , Cmd.none
-                )
-
-            else
-                ( model, Cmd.none )
-
-        SetPlaying bool ->
-            ( { model | playing = bool }
-            , Cmd.none
-            )
-
-        ChangeDirection direction ->
-            let
-                newSnake snake =
-                    { snake | direction = direction }
-            in
-            ( { model | snake = newSnake model.snake }
-            , Cmd.none
-            )
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Time.every 400 Tick
-
-
-
--- VIEW
-
-
-viewLayout : Model -> Html.Html Msg
-viewLayout model =
-    layout [ Background.color (Hex.toColor "#2E3440") ] (view model)
-
-
-view : Model -> Element Msg
-view model =
-    column [ centerX, centerY ]
-        [ board model.size model.snake -- #8FBCBB
-        , button [ Font.color white ]
-            { label =
-                text <|
-                    if model.playing then
-                        "stop"
-
-                    else
-                        "start"
-            , onPress = Just (SetPlaying (not model.playing))
-            }
-        , arrows
-        , currentSnake model.snake.body
-        ]
-
-
-currentSnake : List Point -> Element Msg
-currentSnake list =
-    row [ Font.color white, spaceEvenly, width fill ] <|
-        List.map
-            (\point ->
-                text
-                    (String.fromInt point.x ++ " " ++ String.fromInt point.y)
-            )
-            list
-
-
-white : Color
-white =
-    Hex.toColor "#ECEFF4"
-
-
-arrows : Element Msg
-arrows =
-    row
-        [ Font.size 50
-        , Font.color white
-        , spaceEvenly
-        , width fill
-        ]
-        [ button []
-            { label = text "Left"
-            , onPress = Just (ChangeDirection Left)
-            }
-        , button []
-            { label = text "Right"
-            , onPress = Just (ChangeDirection Right)
-            }
-        , button []
-            { label = text "Up"
-            , onPress = Just (ChangeDirection Up)
-            }
-        , button []
-            { label = text "Down"
-            , onPress = Just (ChangeDirection Down)
-            }
-        ]
+type GameState
+    = Playing
+    | Paused
+    | Lost
 
 
 type alias Snake =
@@ -202,8 +54,8 @@ type alias Snake =
     }
 
 
-startingSnake : Snake
-startingSnake =
+initialSnake : Snake
+initialSnake =
     Snake
         (List.range 1 3
             |> List.map (\n -> Point n 1)
@@ -229,6 +81,248 @@ type alias Size =
     { min : Int
     , max : Int
     }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Tick Time.Posix
+    | SetGameState GameState
+    | ChangeDirection Direction
+    | StartOver
+
+
+validateSnake : Size -> Snake -> Bool
+validateSnake size snake =
+    isSnakeInBounds size snake && not (didEatHimself snake)
+
+
+didEatHimself : Snake -> Bool
+didEatHimself snake =
+    let
+        reversed =
+            List.reverse snake.body
+    in
+    case reversed of
+        x :: xs ->
+            List.member x xs
+
+        _ ->
+            False
+
+
+isSnakeInBounds : Size -> Snake -> Bool
+isSnakeInBounds size snake =
+    (snake.head.x <= size.max)
+        && (snake.head.x >= size.min)
+        && (snake.head.y <= size.max)
+        && (snake.head.y >= size.min)
+
+
+moveSnake : Snake -> Snake
+moveSnake { body, direction, head } =
+    let
+        tail =
+            body |> List.tail |> Maybe.withDefault initialSnake.body
+
+        updateBody newHead =
+            Snake (tail ++ [ newHead ]) newHead direction
+    in
+    case direction of
+        Right ->
+            Point (head.x + 1) head.y |> updateBody
+
+        Left ->
+            Point (head.x - 1) head.y |> updateBody
+
+        Up ->
+            Point head.x (head.y - 1) |> updateBody
+
+        Down ->
+            Point head.x (head.y + 1) |> updateBody
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick _ ->
+            let
+                newSnake =
+                    moveSnake model.snake
+
+                isValid =
+                    validateSnake model.size newSnake
+            in
+            case model.gameState of
+                Playing ->
+                    if isValid then
+                        ( { model | snake = newSnake }
+                        , Cmd.none
+                        )
+
+                    else
+                        ( { model | gameState = Lost }
+                        , Cmd.none
+                        )
+
+                Paused ->
+                    ( model, Cmd.none )
+
+                Lost ->
+                    ( model, Cmd.none )
+
+        SetGameState gameState ->
+            ( { model | gameState = gameState }
+            , Cmd.none
+            )
+
+        ChangeDirection newDirection ->
+            let
+                newSnake snake =
+                    { snake | direction = calcNextDirection snake.direction newDirection }
+            in
+            ( { model | snake = newSnake model.snake }
+            , Cmd.none
+            )
+
+        StartOver ->
+            ( initialModel, Cmd.none )
+
+
+type Side
+    = Horizontal
+    | Vertical
+
+
+mapDirectionToSide : Direction -> Side
+mapDirectionToSide dir =
+    case dir of
+        Left ->
+            Horizontal
+
+        Right ->
+            Horizontal
+
+        Up ->
+            Vertical
+
+        Down ->
+            Vertical
+
+
+calcNextDirection : Direction -> Direction -> Direction
+calcNextDirection prev next =
+    let
+        prevSide =
+            mapDirectionToSide prev
+
+        nextSide =
+            mapDirectionToSide next
+    in
+    case ( prevSide, nextSide ) of
+        ( Horizontal, Vertical ) ->
+            next
+
+        ( Vertical, Horizontal ) ->
+            next
+
+        _ ->
+            prev
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Time.every 400 Tick
+
+
+
+-- VIEW
+
+
+viewLayout : Model -> Html.Html Msg
+viewLayout model =
+    layout [ Background.color (Hex.toColor "#2E3440") ] (view model)
+
+
+view : Model -> Element Msg
+view model =
+    column [ centerX, centerY ]
+        [ board model.size model.snake -- #8FBCBB
+        , playButton model.gameState
+        , arrows model.snake.direction
+        , currentSnake model.snake.body
+        ]
+
+
+playButton : GameState -> Element Msg
+playButton game =
+    let
+        templateButton str state =
+            button []
+                { label = text str
+                , onPress = Just state
+                }
+    in
+    case game of
+        Playing ->
+            templateButton "Pause" (SetGameState Paused)
+
+        Paused ->
+            templateButton "Resume" (SetGameState Playing)
+
+        Lost ->
+            templateButton "Start over" StartOver
+
+
+currentSnake : List Point -> Element Msg
+currentSnake list =
+    row [ Font.color white, spaceEvenly, width fill ] <|
+        List.map
+            (\point ->
+                text
+                    (String.fromInt point.x ++ " " ++ String.fromInt point.y)
+            )
+            list
+
+
+white : Color
+white =
+    Hex.toColor "#ECEFF4"
+
+
+arrows : Direction -> Element Msg
+arrows direction =
+    let
+        templateButton str msg =
+            button []
+                { label = text str
+                , onPress = Just (ChangeDirection msg)
+                }
+    in
+    row
+        [ Font.size 50
+        , Font.color white
+        , spaceEvenly
+        , width fill
+        ]
+        [ templateButton "Left" Left
+        , templateButton "Up" Up
+        , templateButton "Down" Down
+        , templateButton "Right" Right
+        ]
 
 
 board : Size -> Snake -> Element Msg
